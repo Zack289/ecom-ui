6543b020f37239ef7fb24cc0911a1d6c2246f3ea
Zack289 <ozack911@gmail.com>
2026-02-17 18:11:04 +0545

    Added some options in the cart sevtion

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,12 +1,22 @@
 export type ProductType = {
-    id: string | number;
-    name: string;
-    shortDescription: string;
-    description: string;
-    price: number;
-    sizes: string[];
-    colors: string[];
-    images: Record<string, string>;
-}
+  id: string | number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  sizes: string[];
+  colors: string[];
+  images: Record<string, string>;
+};
 
 export type ProductsType = ProductType[];
+
+export type cartItemType = ProductType & {
+  //all from product type and additional more
+  quantity: number;
+  selectedSize: string;
+  selectedColor: string;
+};
+
+
+export type cartItemsType = cartItemType[];
