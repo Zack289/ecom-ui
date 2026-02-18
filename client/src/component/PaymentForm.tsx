dbd71ec8c23100c10c20bd52c8eea61e6d7b1547
Zack289 <ozack911@gmail.com>
2026-02-18 22:03:42 +0545

    Handled the cart page using the zustand

--- client/src/component/PaymentForm.tsx
+++ client/src/component/PaymentForm.tsx
@@ -1,6 +1,116 @@
-const PaymentForm = () => {
+import { PaymentFormInputs, paymentFormSchema } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight, ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const PaymentForm = ({
+
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PaymentFormInputs>({
+    resolver: zodResolver(paymentFormSchema),
+  });
+
+  const router = useRouter();
+
+  const handlePaymentForm: SubmitHandler<PaymentFormInputs> = (data) => {
+   
+  };
+
   return (
-    <div>PaymentForm</div>
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit(handlePaymentForm)}
+    >
+      <div className="flex flex-col gap-1">
+        {/* CARD HOLDER NAME  */}
+        <label htmlFor="cardHolder" className="text-xs text-gray-500 font-medium">
+          Name on Card
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="cardHolder"
+          placeholder="John Snow"
+          {...register("cardHolder")}
+        />
+        {errors.cardHolder && (
+          <p className="text-xs text-red-500">{errors.cardHolder.message}</p>
+        )}
+      </div>
+
+      {/* CARD NUMBER  */}
+      <div className="flex flex-col gap-1">
+        <label htmlFor="cardNumber" className="text-xs text-gray-500 font-medium">
+          Card Number
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="cardNumber"
+          placeholder="123456789..."
+          {...register("cardNumber")}
+        />
+        {errors.cardNumber && (
+          <p className="text-xs text-red-500">{errors.cardNumber.message}</p>
+        )}
+      </div>
+
+      {/* EXPIRATION DATE  */}
+      <div className="flex flex-col gap-1">
+        <label htmlFor="expirationDate" className="text-xs text-gray-500 font-medium">
+          Expiration Date
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="expirationDate"
+          placeholder="MM/YY"
+          {...register("expirationDate")}
+        />
+        {errors.expirationDate && (
+          <p className="text-xs text-red-500">{errors.expirationDate.message}</p>
+        )}
+      </div>
+
+      {/* CVV  */}
+      <div className="flex flex-col gap-1">
+        <label htmlFor="cvv" className="text-xs text-gray-500 font-medium">
+          CVV
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="cvv"
+          placeholder="123"
+          {...register("cvv")}
+        />
+        {errors.cvv && (
+          <p className="text-xs text-red-500">{errors.cvv.message}</p>
+        )}
+      </div>
+
+      {/* CARD IMAGES  */}
+      <div className="flex items-center gap-2 mt-4">
+        {/* <Image src="/klarna.png" alt="" width={50} height={25} className="rounded-md"/> */}
+        <Image src="/cards.png" alt="" width={50} height={25} className="rounded-md"/>
+        <Image src="/stripe.png" alt="" width={50} height={25} className="rounded-md"/>
+      </div>
+
+      {/* BUTTON  */}
+      <button
+        type="submit"
+        className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2"
+      >
+        Check Out
+        <ShoppingCart className="w-3 h-3" />
+      </button>
+    </form>
   );
 };
 
--- client/src/component/ProductCard.tsx
+++ client/src/component/ProductCard.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import useCartStore from "@/stores/cardStore";
 import { ProductType } from "@/types";
 import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
   const [productTypes, setProductTypes] = useState({
     size: product.sizes[0],
     color: product.colors[0],
   });
+
+  const {addToCart} = useCartStore();
+
+  const handleAddToCart = ()=>{
+    addToCart({
+      ...product,
+      quantity: 1,
+      selectedColor: productTypes.color,
+      selectedSize: productTypes.size
+    });
+
+    toast.success("Product added to cart.")
+  }
 
   const handleProductType = ({
     type,
@@ -90,7 +105,7 @@ const ProductCard = ({ product }: { product: ProductType }) => {
         <div className="flex items center justify-between">
           <p className="font-medium">${product.price.toFixed(2)}</p>
 
-          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+          <button onClick={handleAddToCart} className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
             <ShoppingCart className="w-4 h-4" />
             Add to cart
           </button>
